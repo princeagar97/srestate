b6561b56ba0ede35b54e84db91a702d03cf987d5
prince-mastr <63225752+prince-mastr@users.noreply.github.com>
2021-10-26 11:20:56 +0530

    make it better

--- srestate/urls.py
+++ srestate/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/property/', include('property.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
