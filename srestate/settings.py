c537e0b9f2a93ee2e3f11f90276b80a44716c57b
prince-mastr <63225752+prince-mastr@users.noreply.github.com>
2021-10-26 11:16:22 +0530

    make it better

--- srestate/settings.py
+++ srestate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'os*+e_tp3#-yj#5lr^da=2%4!omhenb%$@-emahbe63+qjpa-m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["srestate.herokuapp.com","127.0.0.1:8000"]
 
 
 # Application definition
